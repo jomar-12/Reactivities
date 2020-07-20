5322809a0e72fa3d3760c49f79d843f92cb32290
jomar <jomar@LAPTOP-6QPLTU2S>
2020-07-19 20:53:35 -0400

    Creating API CRUD with mediatR CQRS

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,46 +1,36 @@
-import React, {Component} from 'react';
-import './App.css';
-import { Header, Icon } from 'semantic-ui-react'
-import axios from 'axios';
+import React, { Component } from "react";
+import "./App.css";
+import { Header, Icon, List } from "semantic-ui-react";
+import axios from "axios";
 
 class App extends Component {
   state = {
-    values: []    
-  }
+    values: [],
+  };
 
-  componentDidMount(){
-    
-    axios.get('http://localhost:5000/api/values')
-    .then((response) => {
-
-      console.log(response);
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/values").then((response) => {
       this.setState({
-        values: response.data
-      })
-
-    })
-  
+        values: response.data,
+      });
+    });
   }
 
-  render(){
+  render() {
     return (
-    <div>
-      <Header as='h2'>
-        <Icon name='users' />
+      <div>
+        <Header as="h2">
+          <Icon name="users" />
           <Header.Content>Reactivities</Header.Content>
-      </Header>
-       <ul>
-         {this.state.values.map((value: any) => (
-
-         <li key={value.id}>{value.name}</li>
-
-         ))}
-       </ul>
-
-    </div>
-  );
+        </Header>
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
+      </div>
+    );
   }
-  
 }
 
 export default App;
